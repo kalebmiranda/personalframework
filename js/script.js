54e347459c67dab1d939e7f18293082f1eb8bf78
kalebmiranda <97697576+kalebmiranda@users.noreply.github.com>
2022-10-13 12:36:30 -1000

    added toggle menu

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    // OPEN AND CLOSE SITE NAV  MENU
+    const mysitenav = document.querySelector('.site-nav');
+    const mymenubutton = document.querySelector('.menu-button');
+
+    mymenubutton.onclick = function () {
+        if (mysitenav.getAttribute('data-navstate') === ) {
+            mysitenav.getAttribute('data-navstate','open');
+        } else {
+                mysitenav.setAttribute('data-navstate','closed')
+        };
+    
+    };
+
+
+}); 
